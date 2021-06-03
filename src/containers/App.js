18152ffb42620e84b557ffe77064af5be2afd824
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-06-03 12:38:07 -0500

    :construction: Stateful and Stateless

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import "../assets/styles/containers/App.css";
 
-const App = () => {
-  const addRows = () => [
-    <tr>
-      <td>Jozek</td>
-      <td>hajduksanchez.dev@gmail.com</td>
-      <td>hajduk-sanchez.com</td>
-    </tr>,
-    <tr>
-      <td>Andrzej</td>
-      <td>hajduksanchez.dev@gmail.com</td>
-      <td>hajduk-sanchez.com</td>
-    </tr>,
-  ];
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          name: "Jozek",
+          mail: "jozek@gmail.com",
+          link: "https://hajduk.com",
+        },
+        {
+          name: "Andrzej",
+          mail: "jozekHajduk@gmail.com",
+          link: "https://hajdukJozek.com",
+        },
+      ],
+    };
+  }
+  addRows = () =>
+    this.state.users.map((user) => (
+      <tr>
+        <td>{user.name}</td>
+        <td>{user.mail}</td>
+        <td>{user.link}</td>
+      </tr>
+    ));
 
-  return (
-    <div className='margin'>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Mail</th>
-            <th>Link</th>
-          </tr>
-        </thead>
-        {/* We add the JSX into our HTML */}
-        <tbody>{addRows()}</tbody>
-      </table>
-    </div>
-  );
-};
-
-export default App;
+  render() {
+    return (
+      <div className='margin'>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Mail</th>
+              <th>Link</th>
+            </tr>
+          </thead>
+          {/* We add the JSX into our HTML */}
+          <tbody>{this.addRows()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
