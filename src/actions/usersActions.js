5bca98c41d2c748c08483f09dbc2826c06d5cce4
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-06-04 11:10:02 -0500

    :construction: File types

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import { ALL_USERS } from "../types/usersTypes";
 
 // Is a function into another function
 // The dispatch is going to generate the call when he was called to do a specific action
 export const fetchUsers = () => async (dispatch) => {
   const response = await axios.get("https://jsonplaceholder.typicode.com/users");
   dispatch({
-    type: "gave_users",
+    type: ALL_USERS,
     payload: response.data,
   });
 };
--- src/reducers/usersReducers.js
+++ src/reducers/usersReducers.js
@@ -1,3 +1,5 @@
+import { ALL_USERS } from "../types/usersTypes";
+
 // Initial state of this reducers
 const INITIAL_STATE = {
   users: [],
@@ -6,7 +8,7 @@ const INITIAL_STATE = {
 // The action is an object that we pass with help of the Redux action
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "gave_users":
+    case ALL_USERS:
       return { ...state, users: action.payload };
     default:
       return state;
