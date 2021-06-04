7ba5a4cbf713acd1161807dd0bc15b430e6855dc
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-06-04 11:14:30 -0500

    :construction: try-catch

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -4,9 +4,13 @@ import { ALL_USERS } from "../types/usersTypes";
 // Is a function into another function
 // The dispatch is going to generate the call when he was called to do a specific action
 export const fetchUsers = () => async (dispatch) => {
-  const response = await axios.get("https://jsonplaceholder.typicode.com/users");
-  dispatch({
-    type: ALL_USERS,
-    payload: response.data,
-  });
+  try {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    dispatch({
+      type: ALL_USERS,
+      payload: response.data,
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
