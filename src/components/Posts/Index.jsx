f810b99e24512ecd07c6c1498203243bb1b6807f
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-06-05 17:54:08 -0500

    :construction: Shared reducer

--- src/components/Posts/Index.jsx
+++ src/components/Posts/Index.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as usersActions from "../../actions/usersActions";
 
-export default class Posts extends Component {
+class Posts extends Component {
+  componentDidMount() {
+    if (!this.props.users.length) this.props.fetchUsers();
+  }
+
   render() {
+    console.log(this.props);
     return (
       <div>
+        <h2>Posts by {}</h2>
         {/* This is the way to get the Params that we past by URL */}
         {this.props.match.params.id}
       </div>
     );
   }
 }
+
+const mapStateToProps = (reducers) => {
+  return reducers.usersReducers;
+};
+
+export default connect(mapStateToProps, usersActions)(Posts);
